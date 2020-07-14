765f011c60624e18d3b96a16adc2add4c454d94f
Saida Davis <saihdavis@gmail.com>
2020-07-14 21:18:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   words_array = sentence.split()
+  #new_array.join(" ")
   #new_array = words_array.each do {|word| word.reverse}
  
-  #new_array.join(" ")
-  
 end
